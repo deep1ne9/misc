81af0dc3d19f9c5c3cef4bffd51cc12612f89d13
deep1ne9 <deep1ne@outlook.com>
2021-12-21 12:13:15 -0600

    Added Install_Connect file

---
+++ install_connect.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+########## 	
+
+echo ____________ "Please hang tight ConnectWise is busy installing"
+echo
+echo
+cd  ~/Downloads
+echo
+echo
+echo
+
+sudo installer -pkg "ConnectWiseControl.ClientSetup.pkg" -target /
+
+echo
+echo
+echo
+
+echo ____________ "Connectwise has been successfully installed!"
+
